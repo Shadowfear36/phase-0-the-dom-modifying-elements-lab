39f2ee36645738f8e541329263335dfa8d932d8d
DylanRhinehart <dylanrhinehart@gmail.com>
2022-12-15 09:05:28 -0800

    commit tree b7c4224dab14e6be5c5ab21dea11eb18b22c5876

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.querySelector('main#main');
+main.remove();
+
+const newHeader = document.createElement('H1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = 'Dylan is the champion';
+document.body.appendChild(newHeader);
